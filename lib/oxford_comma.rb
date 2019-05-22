40b9a987566e0e4d83e14833deced062008f249f
Ashley Tolen <tolenashley7@gmail.com>
2019-05-22 21:01:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join()
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array.collect do |array|
+      array.last = "and #{array.last}"
+    end
+    # array.insert(-2, "and")
+    # array = array[0..-1].join(" ")
+    # # first element to second to last element
+    binding.pry
+  else
+    puts "placeholder"
+  end
 end
