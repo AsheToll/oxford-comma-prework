0a5291e36ab07113e4e9ade17780f546bd9a5fbd
Ashley Tolen <tolenashley7@gmail.com>
2019-05-22 21:17:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     array.join()
   elsif array.length == 2
     array.join(" and ")
-  else
+  else array.length >= 3
     array[-1] = "and #{array.last}"
     array.join(", ")
     # array.insert(-2, "and")
